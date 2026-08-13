2a4abbdf93dd8e5c2383b53e96edcb6b2c69338d
MiguelSouza232 <miguelsouza0204@gmail.com>
2026-08-12 22:03:17 -0300

    Adicionado app mobile com internacionalização e persistência de informação com responsividade

--- AM/OlaAndroid/app/src/main/java/com/example/myfirstapplication/Principal.java
+++ AM/OlaAndroid/app/src/main/java/com/example/myfirstapplication/Principal.java
@@ -1,9 +1,8 @@
 package com.example.myfirstapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +10,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Principal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_principal);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
 
-        Button bt = findViewById(R.id.btClique);
-        bt.setOnClickListener(V -> {
-            Toast.makeText(this, "Você clicou no botão", Toast.LENGTH_SHORT).show();
+        Button btOla = findViewById(R.id.bt_Ola);
+        btOla.setOnClickListener(v -> {
+           // abrir nova tela
+            Intent rota = new Intent(this, MainActivity.class);
+            startActivity(rota);
         });
+
+        Button btCiclo = findViewById(R.id.bt_Ciclo);
+        btCiclo.setOnClickListener(v -> {
+            // abrir nova tela
+            Intent rota = new Intent(this, CicloDeVida.class);
+            startActivity(rota);
+        });
+
     }
 }
